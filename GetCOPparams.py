443ee514e1501379d2a5ee7d39c54b45c79f3f2f
barnabuskev <kevin.brownhill@gmail.com>
2016-12-20 15:21:19 +0000

    save calibration data to CSV file

--- GetCOPparams.py
+++ GetCOPparams.py
@@ -30,7 +30,7 @@ cal_re = "calib.*dat"
 # set regular expression to find cop data file
 cop_re = "subj.*.dat"
 
-# setup regular expressions
+# setup regular expression objects
 cal_re_o = re.compile(cal_re)
 cop_re_o = re.compile(cop_re)
 
@@ -44,16 +44,15 @@ cal_df=pd.DataFrame(columns=['session','sensor','slope','slope.se','r.coef','p-v
 seshd = tk_fd.askdirectory(title = 'Open study directory containing sessions')
 if not seshd:
     sys.exit()
-tst = os.walk(seshd)
 for root, dirs, files in os.walk(seshd):
-    # for each directory level
+    # for each directory
     if len(files) > 0:
-        # do stuff with files...
+        # do stuff with files if dir contains any
         # look for calibration file using reg expression
         c_lst = list(filter(cal_re_o.match,files))
         if c_lst:
             # if list c_lst is not empty..
-            # should contain only one file name
+            # should contain only one calibration file
             assert len(c_lst)==1, "more than one calibration file in directory: {}".format(root)
             # open calibration file
             c_pth = os.path.join(root,c_lst[0])
@@ -62,14 +61,29 @@ for root, dirs, files in os.walk(seshd):
             # DO SOMETHING WITH CALIB DATA HERE ***
             cal_dat = tmp['details']
             nxti = len(cal_df.index)
-            # cal_df=pd.DataFrame(columns=['session','sensor','slope','slope.se','r.coef','p-val'])
-            # create row
-            cal_df.loc[nxti] = None
-            cal_df[nxti,'session'] = root
-            print(cal_df)
+            s_ind = 0
+            for sns in cal_dat.keys():
+                # for each sensor..
+                # create empty row
+                cal_df.loc[nxti+s_ind] = None
+                # get session directory
+                cal_df.ix[nxti+s_ind,'session'] = os.path.basename(root)
+                cal_df.ix[nxti+s_ind,'sensor'] = sns
+                cal_df.ix[nxti+s_ind,'slope'] = cal_dat[sns]['m']
+                cal_df.ix[nxti+s_ind,'slope.se'] = cal_dat[sns]['se']
+                cal_df.ix[nxti+s_ind,'r.coef'] = cal_dat[sns]['r']
+                cal_df.ix[nxti+s_ind,'p-val'] = cal_dat[sns]['p']
+                s_ind+=1
+            #cal_df.ix[nxti,'sensor'] =
         # look for cop data file using reg expression
         d_lst = list(filter(cop_re_o.match,files))
         if d_lst:
             # if list d_lst is not empty..
             # DO SOMETHING WITH DATA FILES HERE ***
             pass
+# write results to files
+res_dir = os.path.join(seshd,'results')
+if not(os.path.isdir(res_dir)):
+    os.mkdir(res_dir)
+calib_file = os.path.join(res_dir,'calib_file.csv')
+cal_df.to_csv(calib_file)
