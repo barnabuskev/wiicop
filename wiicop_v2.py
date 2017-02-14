de6947e60dc8b9550d3c78b54e54934bb435d8ee
barnabuskev <kevin.brownhill@gmail.com>
2017-02-14 14:54:16 +0000

    implemented threading to speed up data acquisition

--- wiicop_v2.py
+++ wiicop_v2.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from subprocess import run
 import time
 import threading
 from queue import Queue
@@ -12,6 +13,7 @@ import pandas as pd
 import pyudev
 import xwiimote
 import select
+import configparser
 from WiiCopFunctions import connectBB, calcCOP, procBBdata, txtmenu,\
 get_sessionname, listdirs, get_acq_info, getnsamp, validcode
 from datetime import datetime
@@ -34,7 +36,7 @@ calib_wgts = pd.Series({0:5,1:9,2:16.5})
 # calib_units = 'lbs'
 calib_units = 'Kgs'
 # set the time interval for FuncAnimation (milliseconds)
-anim_interval = 100
+anim_interval = 50
 
 # constants
 # ~~~~~~~~~
@@ -51,9 +53,6 @@ LBS2KGS = 0.453592
 BB_Y = 238
 BB_X = 433
 
-# set up objects for data acquisition
-lock = threading.Lock()
-wii_q = Queue(maxsize=0)
 
 # FUNCTION DEFINITIONS
 def aqc_name(acq_info):
@@ -74,7 +73,7 @@ class wii_thread(threading.Thread):
     def __init__ (self,bb,cal_mod,BB_X,BB_Y):
         threading.Thread.__init__(self)
         self.runflag = True
-        self.storeflag = True
+        self.storeflag = False
         self.n_s = 4
         self.bbdev = xwiimote.iface(bb.sys_path)
         self.p = select.poll()
@@ -90,6 +89,7 @@ class wii_thread(threading.Thread):
         self.cal_mod = cal_mod
         self.BB_X = BB_X
         self.BB_Y = BB_Y
+
     def run(self):
         lock.acquire()
         runflag = self.runflag
@@ -135,7 +135,7 @@ class plot_cop:
         fig_width = 12
         # set fig ratio based on size of bboard rectange whose corners are sensors
         self.fig = plt.figure(figsize=(fig_width, fig_width*BB_Y/BB_X))
-        self.fig.canvas.set_window_title(acqobj.aqc_name())
+        self.fig.canvas.set_window_title(aqc_name(self.acq_info))
         # frameon determines whether background of frame will be drawn
         ax = self.fig.add_axes([0, 0, 1, 1], frameon=False)
         ax.set_xlim(-BB_X/2, BB_X/2), ax.set_xticks([])
@@ -143,12 +143,15 @@ class plot_cop:
         # create a scatter object at initial position 0,0
         self.scat = ax.scatter(0, 0, s=200, lw=0.5, facecolors='green')
         # create text box
-        self.text_h = ax.text(0.02, 0.98, text_start, verticalalignment='top',horizontalalignment='left',
+        self.text_h = ax.text(0.02, 0.98, self.text_start, verticalalignment='top',horizontalalignment='left',
         transform=ax.transAxes, fontsize=12, bbox=dict(facecolor='white'), gid = 'notrec')
         # create timer object
         if self.acq_info['acq_time'] != 'inf':
-            acq_timer = self.fig.canvas.new_timer(interval=acq_time_ms)
-            acq_timer.add_callback(t_event)
+            acq_time_ms = int(self.acq_info['acq_time'])*1000
+            self.acq_timer = self.fig.canvas.new_timer(interval=acq_time_ms)
+            self.acq_timer.add_callback(self.t_event)
+        # attach keypress event handler to figure canvas
+        self.fig.canvas.mpl_connect('key_press_event', self.onkeypress)
 
     def animate(self,cop_i):
         # plot COP
@@ -158,11 +161,11 @@ class plot_cop:
         self.scat.set_offsets(cop)
 
     # Keypress event handler
-    def onkeypress(evt):
+    def onkeypress(self,evt):
         if evt.key==' ':
             # spacebar pressed
             if self.text_h.get_gid()=='notrec':
-                # not recording data...
+                # start recording data...
                 # change colour of dot
                 self.scat.set_facecolors('red')
                 plt.draw()
@@ -170,42 +173,41 @@ class plot_cop:
                 self.text_h.set_gid('rec')
                 if self.acq_info['acq_time'] != 'inf':
                     # timed acquisition - start timer
-                    acq_timer.start()
-                    text_h.set_text('Timed acquisition')
-                    # set acquisition object to store data in array
-                    acqobj.storedat = True
+                    self.acq_timer.start()
+                    self.text_h.set_text('Timed acquisition')
                 else:
                     # manual acq
                     # change instructions
-                    text_h.set_text(text_stop)
-                    # set acquisition object to store data in array
-                    acqobj.storedat = True
+                    self.text_h.set_text(self.text_stop)
+                # set thread to store data in queue
+                lock.acquire()
+                thd.storeflag = True
+                lock.release()
 
-            elif text_h.get_gid()=='rec':
-
-                if acq_info['acq_time'] != 'inf':
+            elif self.text_h.get_gid()=='rec':
+                # stop recording
+                if self.acq_info['acq_time'] != 'inf':
                     # timed acq - do nothing
                     pass
                 else:
                     # recording data, manual acq
-                    acqobj.storedat = False
-                    acqobj.savedatf = True
+                    lock.acquire()
+                    thd.storeflag = False
+                    thd.runflag = False
+                    lock.release()
                     plt.close()
             else:
                 print('error in onkeypress - unrecognised text_h gid')
 
     # callback function for timer
-    def t_event():
-        acqobj.storedat = False
-        acqobj.savedatf = True
-        acq_timer.remove_callback(t_event)
+    def t_event(self):
+        # stop thread queuing data and stop it running
+        lock.acquire()
+        thd.storeflag = False
+        thd.runflag = False
+        lock.release()
+        self.acq_timer.remove_callback(self.t_event)
         plt.close()
-
-
-
-# attach keypress event handler to figure canvas
-cid = fig.canvas.mpl_connect('key_press_event', onkeypress)
-
 
 
 # ~~~~~~~~~~~~~~~
@@ -224,7 +226,6 @@ if bb==None:
     time.sleep(5)
     print('Exiting')
     sys.exit()
-
 
 # SELECT STUDY
 # ~~~~~~~~~~~~
@@ -280,76 +281,80 @@ sesh_path = os.path.join(std_dir,s_dir_nm)
 os.mkdir(sesh_path,mode=0o775)
 
 
-# CALIBRATE BOARD
-# ~~~~~~~~~~~~~~~
-# preallocate array for mean of sensor readings for each calibration weight
-n_calib = len(calib_wgts)
-sens_mean = np.empty([N_S,n_calib])
-print('\n\nStarting calibration sequence...\nApply weights as close as possible to the centre...\n')
-for i_ws in range(n_calib):
-    print('Apply',str(calib_wgts[i_ws]),calib_units,'to balance board\n')
-    input_str = input('Press return when ready...\n\n')
-    # read data
-    sens_dat = procBBdata(bb, getnsamp, smp_size)
-    # print(sens_dat)
-    # for each sensor...
-    for i_s in range(N_S):
-        sens_dat1 = sens_dat[:,i_s]
-        # print out percentage of readings == 0
-        prctzero = sum(sens_dat1==0)/smp_size*100
-        if prctzero > 0:
-            print('Warning: percentage zeros for {0} sensor = {1:.2f}%'.format(SENS_DCT[i_s],prctzero))
-        # detect if all values are for sensor are zero
-        if prctzero > maxpcnt:
-            print('Error: percentage zeros for {0} sensor exceeds maximum ({1:.2f}%).'.format(SENS_DCT[i_s],prctzero))
-            print('Use heavier weight or move board to another location.')
-            print('Exiting')
-            time.sleep(5)
-            sys.exit()
-        else:
-            # get zscores for sensor
-            zscrs = stats.zscore(sens_dat1)
-            # replace those outside threshold with nans
-            sens_dat1[np.absolute(zscrs) > out_thresh] = np.nan
-            # get mean excluding nans.
-            sens_mean[i_s,i_ws] = np.nanmean(sens_dat1)
-
-# For each sensor get a linear model to calibrate data...
-# create dictionary to store results to file and array for model parameters
-# 'm'-slopes,'c'-intercepts, 'p'-p-values, 'r'- r values, 'se' -standard errors
-# cal_mod row zero = slopes, row 1 = intercepts. Each col represents a sensor
-# Calibration weights are divided by number of sensors
-cal_mod = np.empty([2,N_S])
-cal_dat = dict()
-dc = dict()
-for i_s in range(N_S):
-    cal_m, cal_c, cal_r, cal_p, cal_se = stats.linregress(sens_mean[i_s,:],calib_wgts.values/N_S)
-
-    # store results to dictionary
-    dc.update({'m':cal_m})
-    dc.update({'c':cal_c})
-    dc.update({'r':cal_r})
-    dc.update({'p':cal_p})
-    dc.update({'se':cal_se})
-
-    # store model parameters to an array
-    cal_mod[0,i_s] = cal_m
-    cal_mod[1,i_s] = cal_c
-    cal_dat.update({SENS_DCT[i_s]:dc})
-
-# save calibration data in session directory
-calib_dat = {'model':cal_mod, 'details':cal_dat}
-cfn = os.path.join(sesh_path,'calibration_dat')
-with open(cfn,'wb') as fptr:
-    pickle.dump(calib_dat,fptr)
-print('Remove calibration weights from balance board\n\n')
+# # CALIBRATE BOARD
+# # ~~~~~~~~~~~~~~~
+# # preallocate array for mean of sensor readings for each calibration weight
+# n_calib = len(calib_wgts)
+# sens_mean = np.empty([N_S,n_calib])
+# print('\n\nStarting calibration sequence...\nApply weights as close as possible to the centre...\n')
+# for i_ws in range(n_calib):
+#     print('Apply',str(calib_wgts[i_ws]),calib_units,'to balance board\n')
+#     input_str = input('Press return when ready...\n\n')
+#     # read data
+#     sens_dat = procBBdata(bb, getnsamp, smp_size)
+#     # print(sens_dat)
+#     # for each sensor...
+#     for i_s in range(N_S):
+#         sens_dat1 = sens_dat[:,i_s]
+#         # print out percentage of readings == 0
+#         prctzero = sum(sens_dat1==0)/smp_size*100
+#         if prctzero > 0:
+#             print('Warning: percentage zeros for {0} sensor = {1:.2f}%'.format(SENS_DCT[i_s],prctzero))
+#         # detect if all values are for sensor are zero
+#         if prctzero > maxpcnt:
+#             print('Error: percentage zeros for {0} sensor exceeds maximum ({1:.2f}%).'.format(SENS_DCT[i_s],prctzero))
+#             print('Use heavier weight or move board to another location.')
+#             print('Exiting')
+#             time.sleep(5)
+#             sys.exit()
+#         else:
+#             # get zscores for sensor
+#             zscrs = stats.zscore(sens_dat1)
+#             # replace those outside threshold with nans
+#             sens_dat1[np.absolute(zscrs) > out_thresh] = np.nan
+#             # get mean excluding nans.
+#             sens_mean[i_s,i_ws] = np.nanmean(sens_dat1)
+#
+# # For each sensor get a linear model to calibrate data...
+# # create dictionary to store results to file and array for model parameters
+# # 'm'-slopes,'c'-intercepts, 'p'-p-values, 'r'- r values, 'se' -standard errors
+# # cal_mod row zero = slopes, row 1 = intercepts. Each col represents a sensor
+# # Calibration weights are divided by number of sensors
+# cal_mod = np.empty([2,N_S])
+# cal_dat = dict()
+# dc = dict()
+# for i_s in range(N_S):
+#     cal_m, cal_c, cal_r, cal_p, cal_se = stats.linregress(sens_mean[i_s,:],calib_wgts.values/N_S)
+#
+#     # store results to dictionary
+#     dc.update({'m':cal_m})
+#     dc.update({'c':cal_c})
+#     dc.update({'r':cal_r})
+#     dc.update({'p':cal_p})
+#     dc.update({'se':cal_se})
+#
+#     # store model parameters to an array
+#     cal_mod[0,i_s] = cal_m
+#     cal_mod[1,i_s] = cal_c
+#     cal_dat.update({SENS_DCT[i_s]:dc})
+#
+# # save calibration data in session directory
+# calib_dat = {'model':cal_mod, 'details':cal_dat}
+# cfn = os.path.join(sesh_path,'calibration_dat')
+# with open(cfn,'wb') as fptr:
+#     pickle.dump(calib_dat,fptr)
+# print('Remove calibration weights from balance board\n\n')
 
 #TEST overide calibration
 cal_mod = np.array([[0.01776906,0.01645395,0.02366412,0.02252513],[ 0.39208467,-0.7261971,-0.05245845,-3.55288195]])
 
-####################################################################################################################################################################
 # GET SERIES OF ACQUISITIONS
 loop_flag = True
+
+# set up objects for data acquisition
+lock = threading.Lock()
+wii_q = Queue(maxsize=0)
+
 
 while loop_flag:
 
@@ -357,28 +362,25 @@ while loop_flag:
     # {'group': 'case', 'acq_time': 'inf', 'subject_code': '121', 'epoch': 'before'}
     acq_info = get_acq_info(config)
 
-    # get acquisition time in milliseconds if timed
-    if acq_info['acq_time'] != 'inf':
-        acq_time_ms = int(acq_info['acq_time'])*1000
-
     # create plot_cop instance
-    pltcopobj = plot_cop(acq_info)
+    pltcop_obj = plot_cop(acq_info,BB_X,BB_Y)
+    thd = wii_thread(bb,cal_mod,BB_X,BB_Y)
 
-
-
+    # Start thread
+    thd.start()
 
     # PLOT ANIMATION - interval can't be too small or it gives an attribute error
-    animation = FuncAnimation(fig, acqobj.animate, interval=anim_interval)
+    animation = FuncAnimation(pltcop_obj.fig, pltcop_obj.animate,interval=anim_interval)
     plt.show()
 
-    # save data if flag = True
-    print(acqobj.savedatf)
-    if acqobj.savedatf:
-        acqobj.save_bbdat()
-        print('\nSession {}:'.format(s_dir_nm))
-        print('  Data has been saved in file {}\n'.format(acqobj.aqc_name()+'.dat'))
-    else:
-        print('\nNo data has been saved in this acquisition\n')
+    thd.join()
+
+    # get data from queue and write to file
+    acq_data = np.empty((0,4))
+    while not(wii_q.empty()):
+        acq_data = np.vstack((acq_data,wii_q.get()))
+    #***
+    print(acq_data.shape)
 
     # ask user if they wish to do another acquisition
     chc = input('Get another acquisition? (y/n)\n')
@@ -392,26 +394,3 @@ while loop_flag:
         # run('clear')
 
 # END OF ACQUISITION LOOP
-###########################################################################################################################################################################
-
-# read data from the balance board
-thd = wii_thread(bb,cal_mod,BB_X,BB_Y)
-thd.start()
-for ia in range(10):
-    time.sleep(0.1)
-    lock.acquire()
-    toprnt = thd.cop
-    lock.release()
-    print(toprnt)
-lock.acquire()
-thd.runflag = False
-lock.release()
-thd.join()
-
-# get data from queue
-acq_data = np.empty((0,4))
-while not(wii_q.empty()):
-    acq_data = np.vstack((acq_data,wii_q.get()))
-print(acq_data.shape)
-
-print('all done')
